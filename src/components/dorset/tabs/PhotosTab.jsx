eda6d7d31e6ef568c6e3e91af3eb4503ef119d72
Vincent White <vincentlaw1974@gmail.com>
2026-03-21 15:57:39 +0000

    File changes

--- src/components/dorset/tabs/PhotosTab.jsx
+++ src/components/dorset/tabs/PhotosTab.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { uid } from '@/lib/jobUtils';
 import { base44 } from '@/api/base44Client';
 import { Label } from '@/components/ui/label';
