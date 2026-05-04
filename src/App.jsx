7922795429e1ca7a36c84b2d5fd352130cf4bfd2
Vincent White <vincentlaw1974@gmail.com>
2026-05-04 04:55:14 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -39,14 +39,14 @@ const AuthenticatedApp = () => {
 
 function App() {
   return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClientInstance}>
+    <QueryClientProvider client={queryClientInstance}>
+      <AuthProvider>
         <Router>
           <AuthenticatedApp />
         </Router>
         <Toaster />
-      </QueryClientProvider>
-    </AuthProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
